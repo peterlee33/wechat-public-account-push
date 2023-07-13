081d14558ba5dba0456b3521e854d589d7c69bbd
peterlee33 <39454447+peterlee33@users.noreply.github.com>
2023-07-13 22:55:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,11 +29,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝农', year: '1997', date: '05-09',
+          type: '*生日', name: '倩宝宝', year: '1997', date: '05-09',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '宝贝阳', year: '1997', date: '05-09',
+          type: '生日', name: '君乐宝', year: '1996', date: '10-25',
         },
         {
           type: '节日', name: '恋爱纪念日', year: '2023', date: '04-28',
