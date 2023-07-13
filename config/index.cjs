aa25c557722e22914c43298955d5a056f7639f1e
peterlee33 <39454447+peterlee33@users.noreply.github.com>
2023-07-13 22:49:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,56 +8,56 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx2bb70c362f6aa70c',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '25344f2a43484a62ae9d018828e67cef',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '苏州',
+  CITY: '太仓',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      name: '倩宝宝',
+      // 发送给谁？-----------------------------------------------------------------------------------------
+      id: 'obiO96cYcW3LcN7p_Ti04XFuXcoE',
+      // 模板ID-------------------------------------------------------------------------------
+      useTemplateId: 'W2XYk9cXmjofn1BwKxMrCjGb2X5WGZXF-KhKyrapBOI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '05-09',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝农', year: '1997', date: '05-09',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '宝贝阳', year: '1997', date: '05-09',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '恋爱纪念日', year: '2023', date: '04-28',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        // 相识的日子
+        { keyword: 'love_day', date: '2023-03-26' },
+        // 在一起纪念日
+        { keyword: 'marry_day', date: '2023-04-28' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'Y8QDUttjwERqJQaANUVD5rW7QP7oS7ZbFVCXgvo2rQE',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'obiO96cYcW3LcN7p_Ti04XFuXcoE',
     }
   ],
 
